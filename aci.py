8359a938a46671d92cb1a4e19f786074484413fc
jianwei <thucjw@gmail.com>
2015-12-08 03:47:11 -0500

    Update

--- aci.py
+++ aci.py
@@ -22,33 +22,48 @@ def reg_m(y, x):
     return results
 
 
+# params: beta0, beta[last], beta[last-1], ...
 def genData(sigma, params, out_dim, N):
     
-    x = np.zeros((N, out_dim))
+    x = np.ones((N, out_dim))
     y = np.zeros((N, 1))
 
-    coefs = np.zeros((1, out_dim))
-
-    for i in range(out_dim):
-        if i < len(params):
-            coefs[0, i] = params[i]
 
     for i in range(N):
-        x[i, :] = np.random.rand(1, out_dim)
-        y[i] = sum(np.multiply(x[i, :], coefs[0, :])) + np.random.normal(0, sigma)
+        for j in range(out_dim):
+            if j == 0:
+                x[i, j] = 1
+            else:
+                x[i, j] = np.random.normal(0, 1)
+                
+    for i in range(N):
+       for j in range(len(params)):
+           y[i] += x[i][j] * params[j]
+       y[i] += np.random.normal(0, sigma)    
 
-     
     return (x, y)
 
+def compute_residual(params, y, x):
+    params = params[1::-1]
+    result = 0
+    n = len(y)
+    m = len(x[0])
+    for i in range(n):
+        temp = 0
+        for j in range(m):
+            if j < len(params):
+                temp += x[i][j] * params[j]
 
-x, y = genData(1.0, [1, 2, 3], 7, 10)
 
 
-print x.T
+x, y = genData(1, [2, 4, 3], 8, 20)
+
+
 print y.T
-
-result = reg_m(y.T, x.T)
-    
+print x.T
+result = reg_m(y.T[0], x.T[1:4, :])
+print result.summary()
+print result.params
 
 
 
