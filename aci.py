c072d1aeb62a621d720354e3277ce78a9a14b7c3
jianwei <thucjw@gmail.com>
2015-12-08 14:48:32 -0500

    Improvement

--- aci.py
+++ aci.py
@@ -1,3 +1,8 @@
+"""
+Date: December 7 2015
+Author: Huanzhu Xu and Jianwei Cui
+"""
+import math
 import numpy as np
 from scipy import stats
 import statsmodels.api as sm
@@ -10,8 +15,12 @@ N: number of data points
 M: 
 """
 
-def aci(model, Y, X, Np, N):
-    pass
+def computeAIC(residual_error, dim, N):
+    return N * (math.log(residual_error) + 1) + 2 * (dim + 1)
+
+def computeAICC(residual_error, dim, N):
+    return computeAIC(residual_error, dim, N) + 2 * (dim + 1) * (dim + 2) / (N - dim - 2)
+
 
 def reg_m(y, x):
     ones = np.ones(len(x[0]))
@@ -21,49 +30,76 @@ def reg_m(y, x):
     results = sm.OLS(y, X).fit()
     return results
 
+"""
+Input:
+    params: beta0, beta1, beta2, ...
+    N: number of data points
+    out_dim: number of generated input data dimension, including the constant dimension
+    sigma: std. dev. of the noise
+Output:
+    y: a row vector of size N
+    x: data point matrix, with out_dim rows and N columns
 
-# params: beta0, beta[last], beta[last-1], ...
+"""
 def genData(sigma, params, out_dim, N):
-    
-    x = np.ones((N, out_dim))
-    y = np.zeros((N, 1))
+    x = np.ones((out_dim, N))
+    y = np.zeros(N)
 
-
-    for i in range(N):
-        for j in range(out_dim):
-            if j == 0:
+    for i in range(out_dim):
+        for j in range(N):
+            """ first element is always 1 """
+            if i == 0:
                 x[i, j] = 1
             else:
                 x[i, j] = np.random.normal(0, 1)
                 
-    for i in range(N):
-       for j in range(len(params)):
-           y[i] += x[i][j] * params[j]
-       y[i] += np.random.normal(0, sigma)    
+    for j in range(N):
+        for i in range(len(params)):
+           y[j] += x[i][j] * params[i]
+        y[j] += np.random.normal(0, sigma)    
 
     return (x, y)
 
-def compute_residual(params, y, x):
-    params = params[1::-1]
-    result = 0
-    n = len(y)
-    m = len(x[0])
-    for i in range(n):
-        temp = 0
-        for j in range(m):
-            if j < len(params):
-                temp += x[i][j] * params[j]
+def computeResidualError(params, y, x):
+    """
+    Need to reverse so params follow the pattern: beta0, beta1, beta2, beta3
+    """
+    params = params[::-1]
 
+    residual_error = 0.0
+    """ Number of data points """
+    N = len(y)
+    """ Number of effective dimension """
+    dim = len(params)
 
+    for j in range(N):
+        predict = 0
+        for i in range(dim):
+            predict += x[i][j] * params[i]
+        #print "Predict: %f, Real: %f" % (predict, y[j]) 
+        residual_error += (y[j] - predict)**2
 
-x, y = genData(1, [2, 4, 3], 8, 20)
+    return residual_error / N
 
+x, y = genData(1, [.5, 1, 2, 3], 8, 20)
 
-print y.T
-print x.T
-result = reg_m(y.T[0], x.T[1:4, :])
-print result.summary()
-print result.params
+def getModelCriterion(params, y, x, judge_func):
+    sigma_2 = computeResidualError(params, y, x)
+    score = judge_func(sigma_2, len(params), len(y))
+    print "Model score with order: %d is %f" % (len(params), score)
+    return score
+
+print y
+print x
+
+#result = reg_m(y, x[1:4, :])
+#print result.summary()
+#print result.params
+
+for order in range(1, 7):
+    params = reg_m(y, x[1: order + 1, :]).params
+    getModelCriterion(params, y, x, computeAIC)
+
 
 
 
